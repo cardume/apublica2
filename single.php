ecb7a7fb23c9aa66298ad0eb4579e29a9bfd0b2c
monoludens <wil.cavalheiro@gmail.com>
2014-04-12 18:21:52 -0300

    single breadcrumb add

--- single.php
+++ single.php
@@ -1,11 +1,6 @@
-<?php
-
-get_header();
-
-$is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() ); ?>
+<?php get_header(); ?>
 
 <div id="main-content">
-<?php if ( !is_home() && !is_front_page() ): ?>
 	<div class="header-full">
 		<div class="breadcrumb">
 			<p><span><a href="<?php site_URL() ?>">home/ </a></span>
@@ -20,25 +15,18 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() ); ?>
 			<h1><?php echo get_the_title(); ?></h1>
 		</div>
 	</div>
-<?php endif; ?>
-
-
-<?php if ( ! $is_page_builder_used ) : ?>
-
 	<div class="container">
 		<div id="content-area" class="clearfix">
 			<div id="left-area">
-
-<?php endif; ?>
-
 			<?php while ( have_posts() ) : the_post(); ?>
+				<?php if (et_get_option('divi_integration_single_bottom') <> '' && et_get_option('divi_integrate_singlebottom_enable') == 'on') echo(et_get_option('divi_integration_single_bottom')); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'et_pb_post' ); ?>>
+					<h1><?php the_title(); ?></h1>
 
-				<?php if ( ! $is_page_builder_used ) : ?>
-
-					<h1 class="main_title"><?php the_title(); ?></h1>
 				<?php
+					et_divi_post_meta();
+
 					$thumb = '';
 
 					$width = (int) apply_filters( 'et_pb_index_blog_image_width', 1080 );
@@ -49,39 +37,42 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() ); ?>
 					$thumbnail = get_thumbnail( $width, $height, $classtext, $titletext, $titletext, false, 'Blogimage' );
 					$thumb = $thumbnail["thumb"];
 
-					if ( 'on' === et_get_option( 'divi_page_thumbnails', 'false' ) && '' !== $thumb )
+					if ( 'on' === et_get_option( 'divi_thumbnails', 'on' ) && '' !== $thumb )
 						print_thumbnail( $thumb, $thumbnail["use_timthumb"], $titletext, $width, $height );
 				?>
-
-				<?php endif; ?>
 
 					<div class="entry-content">
 					<?php
 						the_content();
 
-						if ( ! $is_page_builder_used )
-							wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
+						wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
 					?>
 					</div> <!-- .entry-content -->
 
-				<?php
-					if ( ! $is_page_builder_used && comments_open() && 'on' === et_get_option( 'divi_show_pagescomments', 'false' ) ) comments_template( '', true );
+					<?php
+					if ( et_get_option('divi_468_enable') == 'on' ){
+						echo '<div class="et-single-post-ad">';
+						if ( et_get_option('divi_468_adsense') <> '' ) echo( et_get_option('divi_468_adsense') );
+						else { ?>
+							<a href="<?php echo esc_url(et_get_option('divi_468_url')); ?>"><img src="<?php echo esc_attr(et_get_option('divi_468_image')); ?>" alt="468 ad" class="foursixeight" /></a>
+				<?php 	}
+						echo '</div> <!-- .et-single-post-ad -->';
+					}
 				?>
 
+					<?php
+						if ( comments_open() && 'on' == et_get_option( 'divi_show_postcomments', 'on' ) )
+							comments_template( '', true );
+					?>
 				</article> <!-- .et_pb_post -->
 
+				<?php if (et_get_option('divi_integration_single_bottom') <> '' && et_get_option('divi_integrate_singlebottom_enable') == 'on') echo(et_get_option('divi_integration_single_bottom')); ?>
 			<?php endwhile; ?>
-
-<?php if ( ! $is_page_builder_used ) : ?>
-
 			</div> <!-- #left-area -->
 
 			<?php get_sidebar(); ?>
 		</div> <!-- #content-area -->
 	</div> <!-- .container -->
-
-<?php endif; ?>
-
 </div> <!-- #main-content -->
 
 <?php get_footer(); ?>
