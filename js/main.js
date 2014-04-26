817006a2dd3f673df8c2177dd282e8817774a071
Miguel Peixe <miguel@cardume.art.br>
2014-04-26 07:14:48 -0300

    support yarpp

--- js/main.js
+++ js/main.js
@@ -6,4 +6,23 @@
 		$('body').fitVids();
 	});
 
+	// Place related posts
+
+	$(document).ready(function() {
+
+		var $yarpp = $('.yarpp-related');
+
+		var after = '.entry-content p:nth-child(4)';
+
+		$(".entry-content p:contains('[relacionados]')").each(function() {
+			$yarpp.insertAfter($(this));
+			$(this).remove();
+			after = false;
+		})
+
+		if(after)
+			$yarpp.insertAfter('.entry-content p:nth-child(4)');
+
+	});
+
 })(jQuery);
