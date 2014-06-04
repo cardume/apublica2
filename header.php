5a4edd419cd167e21d45d7ada8004f1ac02dfe1c
Miguel Peixe <miguel@cardume.art.br>
2014-06-04 13:09:38 -0300

    change app id

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 		  var js, fjs = d.getElementsByTagName(s)[0];
 		  if (d.getElementById(id)) return;
 		  js = d.createElement(s); js.id = id;
-		  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=225271097668783";
+		  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=1458946464351794";
 		  fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
 	</script>
